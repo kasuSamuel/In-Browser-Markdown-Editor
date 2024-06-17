2808283c13b036850ab144e92c100490824216d1
Samuel kasu <kasusamuel17@gmail.com>
2024-06-17 15:20:40 +0000

    defining sone measurement in the  tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Ensure you include all necessary file types and paths
+  ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '10vh': '10vh',
+        '20vh': '20vh',
+        '30vh': '30vh',
+        '40vh': '40vh',
+        '50vh': '50vh',
+        '60vh': '60vh',
+        '70vh': '70vh',
+        '80vh': '80vh',
+        '90vh': '90vh',
+        '100vh': '100vh',
+      },
+    },
   },
   plugins: [],
 }
-
